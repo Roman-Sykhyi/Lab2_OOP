c01ae3aa47604517fcf5a27713f292d9f7fa3e61
Roman-Sykhyi <75393837+Roman-Sykhyi@users.noreply.github.com>
2021-03-02 11:13:08 +0200

    Add files via upload

---
+++ Lab2_OOP.cpp
@@ -0,0 +1,83 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+#include <Windows.h>
+
+using namespace std;
+
+class CharSet
+{
+private:
+	char* str;
+	int strLength;
+
+public:
+	CharSet()
+	{
+		str = new char[1];
+		strLength = 0;
+	}
+
+	CharSet(const char* str)
+	{
+		SetString(str);
+	}
+	~CharSet() 
+	{
+		delete[] str;
+	}
+
+	void SetString(const char* str)
+	{
+		strLength = strlen(str);
+		this->str = new char[strLength+1];
+		strcpy(this->str, str);
+	}
+
+	void Print()
+	{
+		cout << str << endl;
+	}
+
+	bool IsEqualTo(const char* strToCompare)
+	{
+		return strcmp(str, strToCompare);
+	}
+
+	bool IsGreaterThan(const char* strToCompare)
+	{
+		return strlen(str) > strlen(strToCompare);
+	}
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	CharSet charSet("Привіт світ!");
+	//charSet.Set("Привіт світ!");
+
+	charSet.Print();
+
+	if (charSet.IsEqualTo("Привіт світ!") == 0) 
+	{
+		cout << "Рядки однакові" << endl;
+	}
+	else
+	{
+		cout << "Рядки відрізняються" << endl;
+	}
+
+	if (charSet.IsGreaterThan("Привіт світ!"))
+	{
+		cout << "Перший рядок більший за другий" << endl;
+	}
+	else
+	{
+		cout << "Другий рядок більший або рівний з першим" << endl;
+	}
+	
+	system("pause");
+}
